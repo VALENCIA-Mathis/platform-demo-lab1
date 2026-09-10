4a375419d32ed9891af68cfb21ae4a094d877b57
Mathis Valencia <mathisvalencia@MacBook-Pro-de-Mathis.local>
2026-09-10 14:05:24 +0200

    Break version test

--- test/app.test.js
+++ test/app.test.js
@@ -2,4 +2,4 @@ const test=require("node:test");
 const assert=require("node:assert/strict");
 test("root contains service name",()=>assert.equal("platform-demo","platform-demo"));
 test("health is healthy",()=>assert.equal("ok","ok"));
-test("version is correct", () => assert.equal("1.0.0", "1.0.0"));
+test("version is correct", () => assert.equal("1.0.0", "2.0.0"));
